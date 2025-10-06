fe519e9d8383970259777c67fd83650cfd6b009a
Gabriel Matos <dev.matosgabriel@gmail.com>
2025-10-05 21:56:15 -0300

    add Lab 05 finalizado

--- Lab05/script.js
+++ Lab05/script.js
@@ -1,7 +1,6 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d")
 
-
 function quadrados(cor, tamanho, x, y){
     ctx.beginPath();
     ctx.fillStyle = cor;
@@ -82,8 +81,6 @@ quadradosLivres(275, 275, 25, 25, "black")
 quadradosLivres(250, 275, 25, 25, "black")
 quadradosLivres(275, 250, 25, 25, "black")
 
-
-
 function escreverTexto(texto, x, y, corTexto, alinhamento) {
   ctx.beginPath();
   ctx.fillStyle = corTexto;
@@ -92,4 +89,86 @@ function escreverTexto(texto, x, y, corTexto, alinhamento) {
   ctx.fillText(texto, x, y);
   ctx.closePath();
 }
+
 escreverTexto("Canvas", 150, 45, "black", "center");
+
+
+//Canvas casinha
+let canvas2 = document.getElementById("canvas2");
+let ctx2 = canvas2.getContext("2d");
+
+function chaoCasa(cor, x, y, largura, altura){
+  ctx2.beginPath();
+  ctx2.fillStyle = cor;
+  ctx2.fillRect(x, y, largura, altura);
+  ctx2.closePath();
+}
+
+chaoCasa("darkgray", 0, 220, 300, 300)
+
+function arvores(cor, x, y, largura, altura){
+  ctx2.beginPath();
+  ctx2.fillStyle = cor;
+  ctx2.fillRect(x, y, largura, altura);
+  ctx2.closePath();
+}
+
+//arvore1
+arvores("#6d3c03", 260, 220, 15, 45)
+//arvore2
+arvores("#6d3c03", 45, 175, 15, 45)
+
+function elipses(x, y, raio, anguloInicial, anguloFinal, corPreenchimento, lineWidth){
+// Uma aparentemente cachoeira
+  ctx2.beginPath();
+  ctx2.lineWidth = lineWidth;
+  ctx2.fillStyle = corPreenchimento;
+  ctx2.arc(x, y, raio, anguloInicial * Math.PI, anguloFinal * Math.PI);
+  ctx2.fill();
+  ctx2.closePath();
+}
+
+elipses(10, 215, 35, 0, 2, "#5b8cd6ff", 1);
+elipses(118, 293.5, 35, 0, 2, "#5b8cd6ff", 1);
+//sol
+elipses(225, 65, 40, 0, 2, "#fbff13", 1);
+//folha das arvores
+elipses(267, 210, 22, 0, 2, "green", 1);
+elipses(52, 165, 22, 0, 2, "green", 1); 
+
+//parte da coluna da cachoeira
+function colunaCachoeira(x, y, largura, altura, cor){
+  ctx2.beginPath();
+  ctx2.fillStyle = cor;
+  ctx2.fillRect(x, y, largura, altura);
+  ctx2.closePath();
+}
+
+colunaCachoeira(0, 215, 45, 300, "#5b8cd6ff");
+colunaCachoeira(-26, 259, 150, 300, "#5b8cd6ff");
+
+function casa(x, y, largura, altura, cor){
+  ctx2.beginPath();
+  ctx2.fillStyle = cor;
+  ctx2.fillRect(x, y, largura, altura);
+  ctx2.closePath();
+}
+//casa
+casa(110, 130, 90, 90, "#884b06ff");
+//porta
+casa(145, 175, 20, 45, "#472701ff");
+//janelas
+casa(119.5, 150, 25, 25, "#25d3f1ff");
+casa(165, 150, 25, 25, "#25d3f1ff");
+
+function telhado(moveToX, moveToY, lineToX1, lineToY1, lineToX2, lineToY2, cor){
+  ctx2.beginPath();
+  ctx2.moveTo(moveToX, moveToY);
+  ctx2.lineTo(lineToX1, lineToY1);
+  ctx2.lineTo(lineToX2, lineToY2);
+  ctx2.fillStyle = cor;
+  ctx2.fill();
+  ctx2.closePath();
+}
+
+telhado(155, 90, 110, 130, 200, 130, "#ec7e5cff");
